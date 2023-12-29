36b2baded2bf3fecab99ba5081925eab7836180e
Brendo Cruz <43189865+brendocruz@users.noreply.github.com>
2023-12-29 15:43:22 -0300

    add emojis as diagnostic signs

--- lua/lsps/lspconfig.lua
+++ lua/lsps/lspconfig.lua
@@ -67,8 +67,8 @@ vim.lsp.handlers['textDocument/definition'] = goto_definition('split')
 
 
 -- Change diagnostic symbols in the sign column (gutter)
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+local signs = { Error = "🤬", Warn = "🖐️", Hint = "☝️", Info = "🤓" }
 for type, icon in pairs(signs) do
-	local hl = 'DiagnosticSign' .. type
+	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
